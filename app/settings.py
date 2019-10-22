00ab1988782916406ef73da1de9a7014277284fd
Valeriy Andrushchak <waland1510@gmail.com>
2019-10-22 11:25:01 -0400

    even still better

--- app/settings.py
+++ app/settings.py
@@ -98,7 +98,8 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
