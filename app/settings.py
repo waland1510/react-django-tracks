63ae482c30685c1037c1fed7a82c9c6de2efcaab
Valeriy Andrushchak <waland1510@gmail.com>
2019-10-23 09:22:10 -0400

    change settings2

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0','django-react-tracks-gql.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['django-react-tracks-gql.herokuapp.com']
 
 
 # Application definition
