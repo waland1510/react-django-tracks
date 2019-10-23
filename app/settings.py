61f9332680fdec4a08b760118950c300637c7a5e
Valeriy Andrushchak <waland1510@gmail.com>
2019-10-23 08:45:21 -0400

    change settings

--- app/settings.py
+++ app/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_dp!posjfpk2lx!09ofp+lt_o1beh@bn^-=ln0w+_2r8fevu5v'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','django-react-tracks-gql.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
